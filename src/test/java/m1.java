b0b0ed0ead09b3f86e8f99054dc24a673ffca43e
Wangmou <1528297511@qq.com>
2023-06-03 20:43:50 +0800

    first commit

---
+++ src/test/java/m1.java
@@ -0,0 +1,11 @@
+@SuppressWarnings({"all"})
+public class m1 {
+    public static void main(String[] args) {
+        System.out.println("hellow 1");
+        System.out.println("hellow 1");
+        System.out.println("hellow 1");
+        System.out.println("hellow 1");
+        System.out.println("hellow 1");
+        System.out.println("hellow 1");
+    }
+}
