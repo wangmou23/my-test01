024b9d6b26f113ff07fe2967c060540f5d393cfd
Wangmou <1528297511@qq.com>
2023-06-03 20:44:49 +0800

    second commit

--- src/test/java/m1.java
+++ src/test/java/m1.java
@@ -2,7 +2,7 @@
 public class m1 {
     public static void main(String[] args) {
         System.out.println("hellow 1");
-        System.out.println("hellow 1");
+        System.out.println("hellow 2");
         System.out.println("hellow 1");
         System.out.println("hellow 1");
         System.out.println("hellow 1");
